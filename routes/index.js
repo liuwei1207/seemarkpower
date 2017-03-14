2a13083453d6cb04cd2a19a96f34c06dfc4a3085
Lewis <15895891210@sina.cn>
2017-03-14 22:29:59 +0800

    about page

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,40 @@ router.get('/', function(req, res, next) {
     });
 });
 
+/* GET about index page. */
+router.get('/about/index', function(req, res, next) {
+    res.render('./About/index.html', {
+        title: 'seemarkpower'
+    });
+});
+
+/* GET about honor page. */
+router.get('/about/honor', function(req, res, next) {
+    res.render('./About/honor.html', {
+        title: 'seemarkpower'
+    });
+});
+
+/* GET about info page. */
+router.get('/about/info', function(req, res, next) {
+    res.render('./About/info.html', {
+        title: 'seemarkpower'
+    });
+});
+
+/* GET about cnews page. */
+router.get('/about/cnews', function(req, res, next) {
+    res.render('./About/cnews.html', {
+        title: 'seemarkpower'
+    });
+});
+
+/* GET about inews page. */
+router.get('/about/inews', function(req, res, next) {
+    res.render('./About/inews.html', {
+        title: 'seemarkpower'
+    });
+});
+
+
 module.exports = router;
